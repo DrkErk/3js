2e99f1dcd94c2e73a2e59894f8f7ca3aa51f4f8b
ED <erikdrk@gmail.com>
2023-04-07 23:39:35 -0400

    worked on 01-05

--- 01-05/src/script.js
+++ 01-05/src/script.js
@@ -12,11 +12,61 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.y = 1
-scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+cube1.position.x = 1
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = -1
+group.add(cube3)
+
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+//const geometry = new THREE.BoxGeometry(1, 1, 1)
+//const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//const mesh = new THREE.Mesh(geometry, material)
+//
+//position
+//mesh.position.y = 1
+//mesh.position.x = 1
+//mesh.position.z = 1
+//mesh.position.set(1, 1, 1)
+//
+//scale
+//mesh.scale.y = .5
+//mesh.scale.x = .5
+//mesh.scale.z = 2
+//mesh.scale.set(2, .5, .5)
+//
+//
+//Rotation
+//mesh.rotation.reorder('YXZ')
+//mesh.rotation.y = .5
+//mesh.rotation.x = .5
+//
+//
+//scene.add(mesh)
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -31,7 +81,12 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 0
+camera.position.x = 0
 scene.add(camera)
+
+//camera.lookAt(mesh.position)
+
 
 /**
  * Renderer
