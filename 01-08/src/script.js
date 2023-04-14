c080099efe03aa87b8f9117d650f64e2c98b4986
ED <erikdrk@gmail.com>
2023-04-13 22:31:44 -0400

    finished 01-08

--- 01-08/src/script.js
+++ 01-08/src/script.js
@@ -1,5 +1,7 @@
+import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 
 /**
  * Base
@@ -22,9 +24,50 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () =>{
+    //update sizes
+    sizes.width = window.innerWidth,
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+window.addEventListener('dblclick', () =>{
+    const fullscreenElement = document.fullscreenElement || document.webkitFullsceenElement
+    
+    if(!fullscreenElement)
+    {
+        if(canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else
+    {
+        if(document.exitFullscreen())
+        {
+            document.exitFullscreen()
+        }
+        else if(document.webkitExitFullscreen())
+        {
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
 
 /**
  * Camera
@@ -33,6 +76,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -45,6 +90,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio), 5) //2
 
 /**
  * Animate
