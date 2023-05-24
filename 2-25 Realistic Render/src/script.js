1df7cdd4e9c660412e033d13294d5a30527b2b53
ED <erikdrk@gmail.com>
2023-05-23 23:41:32 -0400

    finished 2-25

--- 2-25 Realistic Render/src/script.js
+++ 2-25 Realistic Render/src/script.js
@@ -1,12 +1,22 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,13 +25,86 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test sphere
+ * update all materials
  */
-const testSphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial()
+const updateAllMaterials = () =>
+{
+    scene.traverse((child) =>
+    {
+      if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+      {
+        child.material.envMap = envMap
+        child.material.envMapIntensity = debugObject.envMapIntensity
+        child.castShadow = true
+        child.receiveShadow = true
+      }  
+
+    })
+}
+
+
+/**
+ * ENV MAP
+ */
+
+const envMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+scene.background = envMap
+scene.environment = envMap // applies envmap on all materials out of the gate
+
+debugObject.envMapIntensity = 5
+gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(.001).onChange(updateAllMaterials)
+
+/**
+ * Models
+ */
+gltfLoader.load(
+    '/models/hamburger.glb',
+    (gltf) =>
+    {
+
+        gltf.scene.scale.set(.3, .3, .3)
+        gltf.scene.position.set(0, -1, 0)
+        gltf.scene.rotation.y = Math.PI * .5
+        scene.add(gltf.scene)
+
+        gui
+        .add(gltf.scene.rotation, 'y')
+        .min(-Math.PI)
+        .max(Math.PI)
+        .step(.001)
+        .name('Rotation')
+
+        updateAllMaterials()
+
+    }
 )
-scene.add(testSphere)
+
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.position.set(.25, 3, - 2.25)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024,1024)
+directionalLight.shadow.normalBias = 0.05
+scene.add(directionalLight)
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper)
+
+gui.add(directionalLight, 'intensity').min(0).max(10).step(.01)
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(.001).name('LIGHTX')
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(.001).name('LIGHTY')
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(.001).name('LIGHTZ')
 
 /**
  * Sizes
@@ -62,16 +145,41 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 3
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+gui
+.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping
+})
+.onFinishChange(() =>
+{
+    render.toneMapping = Number(renderer.toneMapping)
+    updateAllMaterials()
+})
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(.001)
 
 /**
  * Animate
  */
 const tick = () =>
 {
+    
+    
+    
     // Update controls
     controls.update()
 
