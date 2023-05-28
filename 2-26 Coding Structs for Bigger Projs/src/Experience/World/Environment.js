b7ff0996ebceddc91e37790ced1069aad2f52bd5
ED <erikdrk@gmail.com>
2023-05-27 23:57:30 -0400

    fin 2-26

--- 2-26 Coding Structs for Bigger Projs/src/Experience/World/Environment.js
+++ 2-26 Coding Structs for Bigger Projs/src/Experience/World/Environment.js
@@ -8,6 +8,13 @@ export default class Environment
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
+        this.debug = this.experience.debug
+
+        //debug
+        if(this.debug.active)
+        {
+            this.debugFolder = this.debug.ui.addFolder('environment')
+        }
         
         this.setSunLight()
         this.setEnvironmentMap()
@@ -22,6 +29,40 @@ export default class Environment
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3.5, 2, - 1.25)
         this.scene.add(this.sunLight)
+
+        //debug
+        if(this.debug.active)
+        {
+            this.debugFolder
+            .add(this.sunLight, 'intensity')
+            .name('sunLightIntensity')
+            .min(0)
+            .max(10)
+            .step(0.001)
+
+            this.debugFolder
+            .add(this.sunLight.position, 'x')
+            .name('sunLightX')
+            .min(-5)
+            .max(5)
+            .step(.001)
+
+            this.debugFolder
+            .add(this.sunLight.position, 'y')
+            .name('sunLightY')
+            .min(-5)
+            .max(5)
+            .step(.001)
+
+            this.debugFolder
+            .add(this.sunLight.position, 'z')
+            .name('sunLightZ')
+            .min(-5)
+            .max(5)
+            .step(.001)
+
+        }
+
     }
 
     setEnvironmentMap()
@@ -33,7 +74,7 @@ export default class Environment
 
         this.scene.environment = this.environmentMap.texture
 
-        this.setEnvironmentMap.updateMaterials = () =>
+        this.environmentMap.updateMaterials = () =>
         {
             this.scene.traverse((child) => 
             {
@@ -44,6 +85,18 @@ export default class Environment
                     child.material.needsUpdate = true
                 }
             })
+        }
+        this.environmentMap.updateMaterials()
+
+        if(this.debug.active)
+        {
+            this.debugFolder
+                .add(this.environmentMap, 'intensity')
+                .name('envMapIntensity')
+                .min(0)
+                .max(4)
+                .step(0.001)
+                .onChange(this.environmentMap.updateMaterials)
         }
 
     }
