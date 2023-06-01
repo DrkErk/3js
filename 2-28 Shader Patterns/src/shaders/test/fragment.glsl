bb2d93ef7bce131ae2981841a161fe0a2a8d3d36
ED <erikdrk@gmail.com>
2023-06-01 00:30:36 -0400

    Finished 2-28

--- 2-28 Shader Patterns/src/shaders/test/fragment.glsl
+++ 2-28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -3,10 +3,10 @@
 varying vec2 vUv;
 
 //pattern 23, 24
-//float random(vec2 st)
-//{
-//    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
-//}
+float random(vec2 st)
+{
+   return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
 
 //pattern 32
 // vec2 rotate(vec2 uv, float rotation, vec2 mid)
@@ -16,6 +16,51 @@ varying vec2 vUv;
 //       cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
 //     );
 // }
+
+// pattern 46 perlin noise
+vec4 permute(vec4 x)
+{
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
+
+vec2 fade(vec2 t)
+{
+    return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+float cnoise(vec2 P)
+{
+    vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+    Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+    vec4 ix = Pi.xzxz;
+    vec4 iy = Pi.yyww;
+    vec4 fx = Pf.xzxz;
+    vec4 fy = Pf.yyww;
+    vec4 i = permute(permute(ix) + iy);
+    vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+    vec4 gy = abs(gx) - 0.5;
+    vec4 tx = floor(gx + 0.5);
+    gx = gx - tx;
+    vec2 g00 = vec2(gx.x,gy.x);
+    vec2 g10 = vec2(gx.y,gy.y);
+    vec2 g01 = vec2(gx.z,gy.z);
+    vec2 g11 = vec2(gx.w,gy.w);
+    vec4 norm = 1.79284291400159 - 0.85373472095314 * vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+    g00 *= norm.x;
+    g01 *= norm.y;
+    g10 *= norm.z;
+    g11 *= norm.w;
+    float n00 = dot(g00, vec2(fx.x, fy.x));
+    float n10 = dot(g10, vec2(fx.y, fy.y));
+    float n01 = dot(g01, vec2(fx.z, fy.z));
+    float n11 = dot(g11, vec2(fx.w, fy.w));
+    vec2 fade_xy = fade(Pf.xy);
+    vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+    float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+    return 2.3 * n_xy;
+}
+
 
 void main()
 {
@@ -127,8 +172,8 @@ void main()
     //gl_FragColor = vec4(str, str, str, 1.0);
 
     //pattern 23
-    //float str = random(vUv);
-    //gl_FragColor = vec4(str, str, str, 1.0);
+    // float str = random(vUv);
+    // gl_FragColor = vec4(str, str, str, 1.0);
 
     //pattern 24
     //vec2 gridUV = vec2(
@@ -272,15 +317,52 @@ void main()
     //
 
     //pattern 45
-    float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
-    angle /= PI * 2.0;
-    angle += 0.5;
-    float sinusoid = sin(angle * 100.0);
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // float sinusoid = sin(angle * 100.0);
+    //
+    // float radius = 0.25 + sinusoid * 0.02;
+    // float str = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+    // gl_FragColor = vec4(str , str, str, 1.0);
 
-    float radius = 0.25 + sinusoid * 0.02;
-    float str = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
-    gl_FragColor = vec4(str , str, str, 1.0);
+    //pattern 46
+    //float str = cnoise(vUv * 10.0);
+    //gl_FragColor = vec4(str , str, str, 1.0);    
 
+    //pattern 47
+    //float str = step(0.0, cnoise(vUv * 10.0));
+    //gl_FragColor = vec4(str , str, str, 1.0);
 
-    //gl_FragColor = vec4(1.0 , 1.0, 1.0, 1.0);
+    //pattern 48
+    //float str = 1.0 - abs(cnoise(vUv * 10.0));
+    //gl_FragColor = vec4(str , str, str, 1.0);
+
+    //pattern 49
+    //float str = sin(cnoise(vUv * 10.0) * 20.0);
+    //gl_FragColor = vec4(str , str, str, 1.0);    
+    
+    //pattern 50
+    //float str = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+    //gl_FragColor = vec4(str , str, str, 1.0);    
+    
+    //pattern 50 with color mixing
+    // float str = step(0.9, sin(cnoise(vUv * 10.0 + 3.3) * 30.0));
+    // vec3 blackColor = vec3(0.0);
+    // vec3 uvColor = vec3(vUv, 1.0);
+    // vec3 mixedColor = mix(blackColor, uvColor, str);
+    // gl_FragColor = vec4(mixedColor, 1.0);
+
+    //CLAMP THE STRENGTH or str using pattern 11 as the example
+    //get the shape
+    float str = step(0.8, mod(vUv.x * 10.0 , 1.0));
+    str += step(0.8, mod(vUv.y * 10.0 , 1.0));
+    //clamped
+    str = clamp(str, 0.0, 1.0);
+    //color mixer
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 mixedColor = mix(blackColor, uvColor, str);
+    gl_FragColor = vec4(mixedColor, 1.0);
+
 }
