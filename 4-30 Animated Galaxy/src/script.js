1043e19057d705f2847b1b0bff935bf275992b6c
ED <erikdrk@gmail.com>
2023-06-07 19:55:38 -0400

    Finished 4-30

--- 4-30 Animated Galaxy/src/script.js
+++ 4-30 Animated Galaxy/src/script.js
@@ -52,6 +52,7 @@ const generateGalaxy = () =>
     const positions = new Float32Array(parameters.count * 3)
     const colors = new Float32Array(parameters.count * 3)
     const scales = new Float32Array(parameters.count * 1)
+    const randomness = new Float32Array(parameters.count * 3)
     
     const insideColor = new THREE.Color(parameters.insideColor)
     const outsideColor = new THREE.Color(parameters.outsideColor)
@@ -65,13 +66,18 @@ const generateGalaxy = () =>
 
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
 
+        //randomness
         const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
         const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
         const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
 
-        positions[i3    ] = Math.cos(branchAngle) * radius + randomX
-        positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
+        randomness[i3 + 0] = randomX
+        randomness[i3 + 1] = randomY
+        randomness[i3 + 2] = randomZ
+
+        positions[i3    ] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0.0 // randomY
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
 
         // Color
         const mixedColor = insideColor.clone()
@@ -88,6 +94,8 @@ const generateGalaxy = () =>
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
     geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
     geometry.setAttribute('aScale', new THREE.BufferAttribute(colors, 1))
+    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
+
 
     /**
      * Material
@@ -100,7 +108,8 @@ const generateGalaxy = () =>
         fragmentShader: galaxyFragmentShader,
         uniforms:
         {
-            uSize: {value: 8 * renderer.getPixelRatio()}
+            uSize: {value: 30 * renderer.getPixelRatio()},
+            uTime: {value: 0}
         }
     })
 
@@ -179,6 +188,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- 4-30 Animated Galaxy/src/shaders/galaxy/fragment.glsl
+++ 4-30 Animated Galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,8 @@
+
+
+varying vec3 vColor;
+
+
         void main()
         {
             // //disc
@@ -15,5 +20,10 @@
             str = 1.0 - str;
             str = pow(str, 10.0);
 
-            gl_FragColor = vec4(vec3(str), 1.0);
+            //final color
+            vec3 color = mix(vec3(0.0), vColor, str);
+
+            gl_FragColor = vec4(color, 1.0);
+            // gl_FragColor = vec4(vColor, str);
+            //can use above and skip color mixing if transparent is true
         }
