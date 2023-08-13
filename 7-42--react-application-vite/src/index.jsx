3b8cba6a6316d43cf2af200edcfcfb951dd111cf
ED <erikdrk@gmail.com>
2023-08-13 16:07:59 -0400

    7-42 last of lil work

--- 7-42--react-application-vite/src/index.jsx
+++ 7-42--react-application-vite/src/index.jsx
@@ -1,11 +1,13 @@
 //- npm create vite@latest
-//-in the root, you can either <></> and let the items fragment or <div></div> to have a parent
-//-not auto closing, you need to always close (ie, <br> needs to be <br /> or related)
+//- in the root, you can either <></> and let the items fragment or <div></div> to have a parent
+//- not auto closing, you need to always close (ie, <br> needs to be <br /> or related)
 //- reserved keywords still exist, in the root, you cant use class or for, instead use className and htmlFor
-// - in {} you can use js and some. {/* is how you comment in the root */}
-//pink {} is "im going to send some js", blue {} is the object
-//use camal case when doing css in js (background-color is backgroundColor)
+//- in {} you can use js and some. {/* is how you comment in the root */}
+//- pink {} is "im going to send some js", blue {} is the object
+//- use camal case when doing css in js (background-color is backgroundColor)
+//- TAILWIND css for jsx (if I want to use it)
 
+import App from './App.jsx'
 import './style.css'
 import { createRoot } from 'react-dom/client'
 
