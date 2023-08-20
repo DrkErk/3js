7245f459443a4227798884b9210071ae3577ba2d
ED <erikdrk@gmail.com>
2023-08-19 23:59:20 -0400

    7-42 test

--- 7-42--react-application-vite/src/index.jsx
+++ 7-42--react-application-vite/src/index.jsx
@@ -14,7 +14,7 @@ import { createRoot } from 'react-dom/client'
 const root = createRoot(document.querySelector('#root'))
 
 const item = "var"
-
+const test = ""
 //inside the app is a render for 
 
 root.render(
