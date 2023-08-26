ea5e3df58175e1814908a0da5a5d83496c450f39
ED <erikdrk@gmail.com>
2023-08-25 23:58:19 -0400

    7-43 orbitcontrol

--- 7-43-first-r3f-app/src/Experience.jsx
+++ 7-43-first-r3f-app/src/Experience.jsx
@@ -1,31 +1,41 @@
 //- You can either do <meshBasicMaterial args={ [ {color: 'red' } ] }/> or below
 //
 //- can do something like <mesh position-x={num} or position={[nums]}
+//
+//- Has the hard way orbit controls
+//
+//- Extend converts things into its declaritive version
 
-import { useFrame } from "@react-three/fiber"
+import {extend, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default function Experience()
 {
     
    const cubeRef = useRef()
+   const groupRef = useRef()
 
-    useFrame(()=>
+    useFrame((state, delta)=>
     {
-        cubeRef.current.rotation.y += 0.01
+        cubeRef.current.rotation.y += delta
+        groupRef.current.rotation.y += (1/2) * delta
     })
     
 
     return <>
         
-        <mesh position-x={ - 2 }>
-            <sphereGeometry/>
-            <meshBasicMaterial color="orange"/>
-        </mesh>
-        <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={ 2 }>
-            <boxGeometry/>
-            <meshBasicMaterial color="mediumpurple" />
-        </mesh>
+        <group ref={ groupRef } >
+            <mesh position-x={ - 2 }>
+                <sphereGeometry/>
+                <meshBasicMaterial color="orange"/>
+            </mesh>
+            <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={ 2 }>
+                <boxGeometry/>
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
+        </group>
+
         <mesh position-y={ -1 } rotation-x={- Math.PI * 0.5} scale = { 10 }>
             <planeGeometry/>
             <meshBasicMaterial color="green" />
