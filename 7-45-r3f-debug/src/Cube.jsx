a872c350bce3171dc0052ba27ef3b46f77e7cd83
ED <erikdrk@gmail.com>
2023-09-03 23:30:10 -0400

    7-45 fin

--- 7-45-r3f-debug/src/Cube.jsx
+++ 7-45-r3f-debug/src/Cube.jsx
@@ -1,7 +1,7 @@
 
 export default function Cube({scale = 1})
 {
-    return         <mesh position-x={ 2 } scale={ scale }>
+    return <mesh position-x={ 2 } scale={ scale } visible='True'>
     <boxGeometry />
     <meshStandardMaterial color="mediumpurple" />
 </mesh>
--- 7-45-r3f-debug/src/index.jsx
+++ 7-45-r3f-debug/src/index.jsx
@@ -1,4 +1,8 @@
 //- strict mode allows for console debug (chrome/firefox)
+//
+//- Leva is react only (most likely)
+//- npm install leva@0.9
+//- npm install r3f-perf@7.1
 
 
 import './style.css'
@@ -6,12 +10,14 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import { StrictMode } from 'react'
+import { Leva } from 'leva'
 
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <StrictMode>
+    <Leva collapsed />
     <Canvas
         camera={ {
             fov: 45,
