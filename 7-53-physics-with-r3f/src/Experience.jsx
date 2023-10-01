6a78bbeb07ab06e10d7bc70e589157043edfd853
ED <erikdrk@gmail.com>
2023-09-30 23:53:57 -0400

    7-53 obj settings

--- 7-53-physics-with-r3f/src/Experience.jsx
+++ 7-53-physics-with-r3f/src/Experience.jsx
@@ -18,10 +18,20 @@
 
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { CuboidCollider, debug, RigidBody, Physics } from '@react-three/rapier'
+import {BallCollider, CuboidCollider, RigidBody, Physics } from '@react-three/rapier'
+import { useRef } from 'react'
 
 export default function Experience()
 {
+
+    const cube = useRef()
+
+    const cubeJump = () =>
+    {
+        cube.current.applyImpulse({ x:0, y:5, z:0 })
+        cube.current.applyTorqueImpulse({ x:Math.random() - 0.5 , y: Math.random() - 0.5 , z: Math.random() - 0.5 })
+    }
+
     return <>
 
         <Perf position="top-left" />
@@ -31,15 +41,27 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
+        <Physics gravity={[0, - 9.08, 0]} />
+
         <Physics debug>
 
             <RigidBody colliders='ball'>
-            <mesh castShadow position={ [ 0, 4, 0 ] }>
+            <mesh castShadow position={ [-1.5, 2, 0 ] }>
                 <sphereGeometry />
                 <meshStandardMaterial color="orange" />
             </mesh>
             </RigidBody>
 
+            {/* for bounciness, restitution, to get trampoline effect, you need rest of 2. custom manual colliders would need coefficentCombineRule and choice one of the provided rules*/}
+            {/* https://rapier.rs/javascript3d/classes/RigidBody.html (more methods for the rigid body) (add force is long lasting, add impulse is short force) */}
+            {/* mass doesnt increase fall speed. */}
+            <RigidBody ref={ cube } position={ [ 1.5, 2, 0 ] } gravityScale={ 1 } restitution={ 0 } friction={0.7} >
+            <mesh castShadow onClick={ cubeJump } >
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+            </RigidBody>
+            
             {/* <RigidBody>
             <mesh castShadow position={ [ 2, 2, 0 ] }>
                 <boxGeometry args={[3,2,1]} />
@@ -53,15 +75,20 @@ export default function Experience()
 
                 {/* hull is like an elastic membrane around an object. the hull should be a convex hull for the torus */}
                 {/* avoid trimesh with dynamic rigid bodies, trimesh colliders are hollow o nthe inside and makes collision detection more difficult and buggy */}
-            <RigidBody colliders={false} >
-            <CuboidCollider arg={[1,1,1]} />
-            <mesh castShadow position={ [ 0,1,0] } rotation={[ Math.PI * 0.5, 0, 0]}>
+                {/* Scale not supported on the rigid body */}
+                {/* colliders stack and you cant subdivide ballColliders*/}
+                {/* HeightfieldColliders are mainly for landscapes you need an array of height values and providing a num of cols and rows*/}
+            {/* <RigidBody colliders={false} position={ [ 0,1,0] } rotation={[ Math.PI * 0.5, 0, 0]}>
+            <BallCollider args={[1.5]} />
+            <CuboidCollider args={[1.5,1.5,0.5]} />
+            <CuboidCollider args={[0.25, 1, 0.25]} position={[0,0,1]} rotation={[ - Math.PI * 0.35, 0, 0]} />
+            <mesh castShadow >
                 <torusGeometry args={[ 1, 0.5, 16, 32]} />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh> 
-            </RigidBody>
+            </RigidBody> */}
 
-            <RigidBody type="fixed">
+            <RigidBody type="fixed" restitution={0} friction={0.7}>
             <mesh receiveShadow position-y={ - 1.25 }>
                 <boxGeometry args={ [ 10, 0.5, 10 ] } />
                 <meshStandardMaterial color="greenyellow" />
