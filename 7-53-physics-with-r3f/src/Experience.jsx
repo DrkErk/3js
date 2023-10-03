37eeca861177ef117d9c57a790c59e473c7dfbb9
ED <erikdrk@gmail.com>
2023-10-02 21:24:12 -0400

    7-53 events

--- 7-53-physics-with-r3f/src/Experience.jsx
+++ 7-53-physics-with-r3f/src/Experience.jsx
@@ -40,9 +40,15 @@ export default function Experience()
     {
         const time = state.clock.getElapsedTime()
 
-        const eulerRotation = new THREE.Euler(0, time, 0)
+        const eulerRotation = new THREE.Euler(0, time * 3, 0)
         const quaternionRotation = new THREE.Quaternion()
         quaternionRotation.setFromEuler(eulerRotation)
+        twister.current.setNextKinematicRotation(quaternionRotation)
+
+        const angle = time * 0.5
+        const x = Math.cos(angle) * 2
+        const z = Math.sin(angle) * 2
+        twister.current.setNextKinematicTranslation({ x: x, y: - 0.8, z: z})
     })
 
     return <>
@@ -111,6 +117,7 @@ export default function Experience()
  
             <RigidBody ref={twister} position={[ 0, - 0.8, 0]} friction={ 0 } type="kinematicPosition">
             <mesh castShadow scale={[ 0.4, 0.4, 3]}>
+                <boxGeometry />
                 <meshStandardMaterial color='red' />
             </mesh>
             </RigidBody>
