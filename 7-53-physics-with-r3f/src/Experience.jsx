b1667c4ff14eecaeb6501dad82d31026fbf4eda8
ED <erikdrk@gmail.com>
2023-10-03 22:18:19 -0400

    7-53 fin

--- 7-53-physics-with-r3f/src/Experience.jsx
+++ 7-53-physics-with-r3f/src/Experience.jsx
@@ -16,16 +16,17 @@
 // ^- Rapier isn't bound to three.js
 
 
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import {BallCollider, CuboidCollider, RigidBody, Physics } from '@react-three/rapier'
-import { useRef } from 'react'
+import { InstancedRigidBodies, CylinderCollider, BallCollider, CuboidCollider, RigidBody, Physics } from '@react-three/rapier'
+import { useMemo, useEffect, useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
 export default function Experience()
 {
 
+    const [hitSound] = useState(() => new Audio('./hit.mp3'))
     const twister = useRef()
     const cube = useRef()
 
@@ -51,6 +52,52 @@ export default function Experience()
         twister.current.setNextKinematicTranslation({ x: x, y: - 0.8, z: z})
     })
 
+    const collisionEnter = () =>
+    {
+        // hitSound.currentTime = 0
+        // hitSound.volume = Math.random()
+        // hitSound.play()
+    }
+
+    const hamburger = useGLTF('./hamburger.glb')
+
+    const cubeCount = 100
+    const instances = useMemo(() =>
+    {
+        const instances = []
+        for(let i = 0; i < cubeCount; i++)
+         {
+            //instances need 3 things, key, position, rotation
+            instances.push({
+                key: 'instance_' + i,
+                position: [(Math.random() - 0.5) * 8, 6 + i * 0.2 , (Math.random() - 0.5) * 8],
+                rotation: [0,0,0]
+
+            })
+         }
+
+        return instances
+    },[])
+
+   // const cubes = useRef()
+   //
+   //
+   //
+    // InstancedRigidBodies does most of this
+    //
+    // useEffect(()=> {
+    //     for(let i = 0; i < cubeCount; i++)
+    //     {
+    //         const matrix = new THREE.Matrix4()
+    //         matrix.compose( 
+    //             new THREE.Vector3(i * 2, 0, 0),
+    //             new THREE.Quaternion(),
+    //             new THREE.Vector3(1,1,1)
+    //         )
+    //         cubes.current.setMatrixAt(i, matrix)
+    //     }
+    // }, [])
+
     return <>
 
         <Perf position="top-left" />
@@ -60,9 +107,7 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <Physics gravity={[0, - 9.08, 0]} />
-
-        <Physics debug>
+        <Physics debug={false} gravity={[0, - 9.08, 0]} >
 
             <RigidBody colliders='ball'>
             <mesh castShadow position={ [-1.5, 2, 0 ] }>
@@ -74,7 +119,16 @@ export default function Experience()
             {/* for bounciness, restitution, to get trampoline effect, you need rest of 2. custom manual colliders would need coefficentCombineRule and choice one of the provided rules*/}
             {/* https://rapier.rs/javascript3d/classes/RigidBody.html (more methods for the rigid body) (add force is long lasting, add impulse is short force) */}
             {/* mass doesnt increase fall speed. */}
-            <RigidBody ref={ cube } position={ [ 1.5, 2, 0 ] } gravityScale={ 1 } restitution={ 0 } friction={0.7} colliders={false} >
+            <RigidBody 
+            ref={ cube } 
+            position={ [ 1.5, 2, 0 ] } 
+            gravityScale={ 1 } restitution={ 0 } 
+            friction={0.7} colliders={false} 
+            // onCollisionEnter={ collisionEnter } 
+            // onCollisionExit={ () => {console.log('exit')}} 
+            // onSleep = {()=> {console.log('sleep')}}
+            // onWake = {()=> {console.log('wake')}}
+            >
             <mesh castShadow onClick={ cubeJump } >
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
@@ -122,7 +176,28 @@ export default function Experience()
             </mesh>
             </RigidBody>
             
+            <RigidBody colliders="trimesh" position={ [0, 4, 0] } >
+                <primitive object={ hamburger.scene } scale={0.25} />
+                {/* <CylinderCollider args={ [0.5, 1.25] } /> */}
+            </RigidBody>
+
+            <RigidBody>
+                <CuboidCollider args={[5,2,0.5]} position={[0,1,5.25]} />
+                <CuboidCollider args={[5,2,0.5]} position={[0,1,-5.25]} />
+                <CuboidCollider args={[0.5,2,5]} position={[5.25,1,0]} />
+                <CuboidCollider args={[0.5,2,5]} position={[-5.25,1,0]} />
+            </RigidBody>
+
+            <InstancedRigidBodies instances={instances} >
+            <instancedMesh castShadow  args={[null, null, cubeCount]}>
+            <boxGeometry />
+            <meshStandardMaterial color='tomato' />
+            </instancedMesh>
+            </InstancedRigidBodies>
+
         </Physics>
+
+
 
     </>
 }
