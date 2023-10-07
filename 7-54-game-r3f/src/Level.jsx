ed0fde6f249bae7e5e5d5791e43fb811f15da910
ED <erikdrk@gmail.com>
2023-10-06 23:51:43 -0400

    7-54 usestate

--- 7-54-game-r3f/src/Level.jsx
+++ 7-54-game-r3f/src/Level.jsx
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { RigidBody } from '@react-three/rapier'
-import { useRef } from 'react'
+import { useState, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 
 const boxGeometry = new THREE.BoxGeometry(1,1,1)
 
@@ -22,10 +23,22 @@ function BlockStart({position=[0,0,0]})
 function BlockSpinner({position = [0,0,0]})
 {
     const obstacle = useRef()
+    const [speed] = useState(()=> Math.random() + 0.2)
+
+    useFrame((state) =>
+    {
+        const time = state.clock.getElapsedTime()
+        const rotation = new THREE.Quaternion()
+        rotation.setFromEuler(new THREE.Euler(0,time * speed,0))
+        obstacle.current.setNextKinematicRotation(rotation)
+        
+    })
+
 
     return <group position={position}>
     <mesh geometry={boxGeometry} material={floor2Material} position={[0,-0.1,0]} scale={[4, 0.2, 4]} receiveShadow />
-        <RigidBody type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+
+        <RigidBody ref={obstacle} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0}>
             <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} />
         </RigidBody>
     </group>
