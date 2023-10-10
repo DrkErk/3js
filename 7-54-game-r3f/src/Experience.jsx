1ad28a87fa1afb0065d625f8b131c95fbd16a4dc
ED <erikdrk@gmail.com>
2023-10-09 22:52:26 -0400

    7-54 player

--- 7-54-game-r3f/src/Experience.jsx
+++ 7-54-game-r3f/src/Experience.jsx
@@ -7,7 +7,8 @@
 import { OrbitControls } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
 import Lights from './Lights.jsx'
-import Level from './Level.jsx'
+import {Level, BlockSpinner} from './Level.jsx'
+import Player from './Player.jsx'
 
 export default function Experience()
 {
@@ -18,6 +19,7 @@ export default function Experience()
         <Physics debug>
         <Lights />
         <Level />
+        <Player />
         </Physics>
 
 
