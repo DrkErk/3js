971b9d89a7636b470b429232e73c2001fc90c5e7
ED <erikdrk@gmail.com>
2023-10-18 22:40:09 -0400

    7-54 fin

--- 7-54-game-r3f/src/Experience.jsx
+++ 7-54-game-r3f/src/Experience.jsx
@@ -1,5 +1,5 @@
 //npm install @react-three/rapier@1.0
-//
+//npm install zustand@4.3
 //
 //
 //
@@ -9,17 +9,24 @@ import { Physics } from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import {Level, BlockSpinner} from './Level.jsx'
 import Player from './Player.jsx'
+import useGame from './store/useGame.jsx'
 
 export default function Experience()
 {
+
+    const blocksCount = useGame((state) =>  state.blocksCount)
+    const blocksSeed = useGame((state) =>  state.blocksSeed)
+
+
     return <>
 
-        <OrbitControls makeDefault />
+        {/* <OrbitControls makeDefault /> */}
+        <color args={ ['#bdedfc'] } attach="background" />
 
         <Physics debug={false}>
-        <Lights />
-        <Level />
-        <Player />
+            <Lights />
+            <Level count={blocksCount}/>
+            <Player />
         </Physics>
 
 
