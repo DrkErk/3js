141f6305db8b7234b673081ff69090115c3a6aa1
ED <erikdrk@gmail.com>
2023-12-16 19:20:51 -0500

    added arraycam example code to de construct

--- 3js Demos/src/Main.jsx
+++ 3js Demos/src/Main.jsx
@@ -1,10 +1,11 @@
 import './style.css'
-//import App from './App.jsx'
+import App from './App.jsx'
 import ExperienceHandler from './ExperienceHandler.jsx'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
