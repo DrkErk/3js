2106ee613d7a7f67a0ae2beb03340d483c5d7cf5
ED <erikdrk@gmail.com>
2024-03-11 19:50:59 -0400

    hologram shader fresnel fix1

--- 3jsJourney/4-33 Hologram/src/shaders/holographic/fragment.glsl
+++ 3jsJourney/4-33 Hologram/src/shaders/holographic/fragment.glsl
@@ -1,6 +1,7 @@
 uniform float uTime;
 
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main()
 {
@@ -9,6 +10,10 @@ void main()
 //stripes
 float stripes = mod((vPosition.y + uTime * -0.02) * 20.0, 1.0 );
 stripes = pow(stripes, 3.0); //sharper curve, so darker to black and to pure white before reset
+
+//fresnel
+vec3 viewDirection = normalize(vPosition - cameraPosition);
+float fresnel = dot(viewDirection, vNormal);
 
 //final color
 gl_FragColor = vec4(1.0, 1.0, 1.0, stripes);
--- 3jsJourney/4-33 Hologram/src/shaders/holographic/vertex.glsl
+++ 3jsJourney/4-33 Hologram/src/shaders/holographic/vertex.glsl
@@ -1,5 +1,6 @@
 
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main()
 {
@@ -9,9 +10,17 @@ void main()
     // final pos
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+    //model normal.  to make sure the fresnel doesn't move with the rotation
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+    // When the forth positon(alpha) is 1.0, the vector is homogeneous and will apply all 3 transformation (translation, rotation, scale)
+    // When the forth positon(alpha) is 0.0, the vector isn't homogeneous and all 3 transformation wont be applied
+    // ^-- we dont want the translation to be applied because the normal isnt a position. It's a direction.
+    
+    
     //varying
     vPosition = modelPosition.xyz;// lines follow the viewmodel, vs position.xyz would be static lines 
- 
+    vNormal = modelNormal.xyz;
+    
 }
 
 
