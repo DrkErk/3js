1c35277141915fd41037c3b3ed62ef64694f21dd
ED <erikdrk@gmail.com>
2024-03-28 21:49:56 -0400

    to firework twinkling

--- 3jsJourney/4-34 fireworks/src/shaders/firework/vertex.glsl
+++ 3jsJourney/4-34 fireworks/src/shaders/firework/vertex.glsl
@@ -14,7 +14,29 @@ void main()
 
 vec3 newPosition = position;
 
-//exploding the progress
+//exploding the progress\
+float explodingProgress = remap(uProgress, 0.0, 0.1, 0.0, 1.0); // remap the input function to work as we want it.
+explodingProgress = clamp(explodingProgress, 0.0, 1.0); // keep it from zero to 1
+explodingProgress = 1.0 - pow(1.0 - explodingProgress, 3.0); // 1-(1-x)^a makes a fast growth that decays and caps at 1
+newPosition *= explodingProgress;
+
+//Falling
+float fallingProgress = remap(uProgress, 0.1, 1.0, 0.0, 1.0);
+fallingProgress = clamp(fallingProgress, 0.0, 1.0);
+fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
+newPosition.y -= fallingProgress * 0.2;
+
+//scaling
+float sizeOpeningProgress = remap(uProgress, 0.0, 0.125, 0.0, 1.0);
+float sizeClosingProgress = remap(uProgress, 0.125, 1.0, 1.0, 0.0);
+float sizeProgress = min(sizeOpeningProgress, sizeClosingProgress);
+sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
+//twinkling
+float twinklingProgress = remap(uProgress, 0.2, 0.8, 0.0, 1.0);
+twinklingProgress = clamp(twinklingProgress, 0.0, 1.0);
+float sizeTwinkling = sin(uProgress * 30.0) * 0.5 + 0.5;
+
 
 //final position
 vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
@@ -24,7 +46,7 @@ gl_Position = projectionMatrix * viewPosition;
 // One line version of above ( gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0); )
 
 //final size
-gl_PointSize = uSize * uResolution.y * aSize; // modern builds make it so that it only needs to resize y when it gets smaller not x
+gl_PointSize = uSize * uResolution.y * aSize * sizeProgress * sizeTwinkling; // modern builds make it so that it only needs to resize y when it gets smaller not x
 gl_PointSize *= 1.0 / - viewPosition.z; // makes the particles grow and shrink based on how far they are
 //Dont forget, pixel ratio will affect the size
 
