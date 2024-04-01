a7f0193e825b932f289185f2e95554381a8cf786
ED <erikdrk@gmail.com>
2024-03-31 21:22:50 -0400

    light shader start directional light

--- 3jsJourney/4-35 Lights Shading/src/shaders/shading/fragment.glsl
+++ 3jsJourney/4-35 Lights Shading/src/shaders/shading/fragment.glsl
@@ -1,8 +1,34 @@
+//for directional light lightPosition: we want the direction and not the position. The difference is that direction is normalized.
+
 uniform vec3 uColor;
+
+varying vec3 vNormal;
+
+#include ../includes/ambientLight.glsl
+
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition)
+{
+    return lightColor * lightIntensity;
+}
 
 void main()
 {
     vec3 color = uColor;
+
+    vec3 light = vec3(0.0);
+
+    // light += ambientLight( 
+        // vec3(1.0, 0.0, 0.0), //light color
+        // 0.2);                // Light intensity
+    light += directionalLight( 
+        vec3(0.1, 0.1, 1.0),    // Light Color
+        1.0,                    // Light Intensity
+        vNormal,                // normal
+        vec3(0.0, 0.0, 3.0)     // Light Position
+
+        );
+
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
