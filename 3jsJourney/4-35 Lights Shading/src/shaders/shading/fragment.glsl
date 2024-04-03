b918ac1a35904e7a621ffcccf07c7c6caf96a44d
ED <erikdrk@gmail.com>
2024-04-02 22:09:34 -0400

    light shader  directional light specular

--- 3jsJourney/4-35 Lights Shading/src/shaders/shading/fragment.glsl
+++ 3jsJourney/4-35 Lights Shading/src/shaders/shading/fragment.glsl
@@ -1,8 +1,14 @@
 //for directional light lightPosition: we want the direction and not the position. The difference is that direction is normalized.
+// //
+// Specular is the light reflection
+// //
+
+ //camera position built in uniform from 3js
 
 uniform vec3 uColor;
 
 varying vec3 vNormal;
+varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 
@@ -12,20 +18,23 @@ vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 l
 
     //shading
     float shading = dot(normal, lightDirection);
+    shading = max(0.0, shading); //Clamp the normal value so when shading is less then zero in cases where it is facing away, it cant go
+                                 // below zero and mess with any other shadings.
 
     //return lightColor * lightIntensity 
-    return vec3(shading);
+    return vec3(lightColor * lightIntensity * shading);
 }
 
 void main()
 {
+    vec3 viewDirection = normalize(vPosition - cameraPosition)
     vec3 color = uColor;
 
     vec3 light = vec3(0.0);
 
-    // light += ambientLight( 
-        // vec3(1.0, 0.0, 0.0), //light color
-        // 0.2);                // Light intensity
+    light += ambientLight( 
+        vec3(1.0, 0.0, 0.0), //light color
+        0.2);                // Light intensity
     light += directionalLight( 
         vec3(0.1, 0.1, 1.0),    // Light Color
         1.0,                    // Light Intensity
--- 3jsJourney/4-35 Lights Shading/src/shaders/shading/vertex.glsl
+++ 3jsJourney/4-35 Lights Shading/src/shaders/shading/vertex.glsl
@@ -1,4 +1,5 @@
 varying vec3 vNormal;
+varying vec3 vPosition; // frag pos from 
 
 void main()
 {
@@ -6,7 +7,12 @@ void main()
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+    //model normal
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0); //once again, applying the model matrix on the normal, set the 4th value to 0.0
+                                                        //and save it as a vec4 modelNormal
+
     //varying 
-    vNormal = normal;
+    vNormal = modelNormal.xyz;
+    vPosition = modelPosition.xyz;
 
 }
