705be12b57356ccfa10a85dcfff673d4a0a2682b
ED <erikdrk@gmail.com>
2024-04-28 22:29:11 -0400

    3js 37 halftone fin

--- 3jsJourney/4-37 HalfTone Shader/src/shaders/halftone/fragment.glsl
+++ 3jsJourney/4-37 HalfTone Shader/src/shaders/halftone/fragment.glsl
@@ -1,11 +1,40 @@
 uniform vec3 uColor;
 uniform vec2 uResolution;
+uniform float uShadowRepetitions;
+uniform vec3 uShadowColor;
+uniform float uLightRepetitions;
+uniform vec3 uLightColor;
 
 varying vec3 vNormal;
 varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
+
+vec3 halftone(
+    vec3 color,
+    float repetitions,
+    vec3 direction,
+    float low,
+    float high,
+    vec3 pointColor,
+    vec3 normal
+)
+{
+ 
+float intensity = dot(normal, direction); // set half tone intensity (dot prod of obj normal and direction of the vec3 below)
+intensity = smoothstep(low, high, intensity);
+
+vec2 uv = gl_FragCoord.xy / uResolution.y; // uResolution.xy makes it the shape of the screen
+uv *= repetitions;
+uv = mod(uv, 1.0);
+
+float point = distance(uv, vec2(0.5));
+point = 1.0 - step(0.5 * intensity, point);
+
+    
+return mix(color, pointColor, point);
+}
 
 
 void main()
@@ -34,18 +63,30 @@ void main()
     color *= light;
 
     // halftone
-    float repetitions = 10.0;
+    color = halftone(
+        color,                  // input color
+        uShadowRepetitions,     // repititions
+        vec3(0.0, -1.0, 0.0),   // direction
+        -0.8,                   // low
+        1.5,                    // high
+        uShadowColor,           // point color
+        normal                  // normal
+    );
 
-    vec2 uv = gl_FragCoord.xy / uResolution.y; // uResolution.xy makes it the shape of the screen
-    uv *= repetitions;
-    uv = mod(uv, 1.0);
+    color = halftone(
+        color,                  // input color
+        uLightRepetitions,     // repititions
+        vec3(1.0, 1.0, 0.0),   // direction
+        -0.8,                   // low
+        1.5,                    // high
+        uLightColor,           // point color
+        normal                  // normal
+    );
 
-    float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5, point);
 
 
     // Final color
-    gl_FragColor = vec4(point, point, point , 1.0);
+    gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
