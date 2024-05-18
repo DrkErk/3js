e8fa9f7ac203c61859c4760d3ec0595c91b49404
ED <erikdrk@gmail.com>
2024-05-17 23:53:16 -0400

    3js 41 gpgpu flow field still

--- 3jsJourney/4-41 GPGPU Flow Field Particles/src/shaders/gpgpu/particles.glsl
+++ 3jsJourney/4-41 GPGPU Flow Field Particles/src/shaders/gpgpu/particles.glsl
@@ -1,12 +1,22 @@
 //uniform sampler2D uParticles; already done by gpu comp renderer
 
+#include ../includes/simplexNoise4d.glsl
 
 void main()
 {
 
 vec2 uv = gl_FragCoord.xy / resolution.xy;
 vec4 particle = texture(uParticles, uv);
-particle.x += 0.01;
+
+//flow field
+vec3 flowField = vec3(
+    simplexNoise4d(vec4(particle.xyz + 0.0, 0.0)),
+    simplexNoise4d(vec4(particle.xyz + 1.0, 0.0)),
+    simplexNoise4d(vec4(particle.xyz + 2.0, 0.0))
+)
+flowField = normalize(flowField);
+particle.xyz += flowField * 0.01;
+
 gl_FragColor = particle;
 
 }
