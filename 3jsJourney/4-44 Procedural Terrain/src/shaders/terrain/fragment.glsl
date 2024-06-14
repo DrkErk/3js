31b680b4b00452ae4b8fdd10c8bf919c0bc432b0
ED <erikdrk@gmail.com>
2024-06-13 22:35:09 -0400

    3js journ 44 to proc terr to color add

--- 3jsJourney/4-44 Procedural Terrain/src/shaders/terrain/fragment.glsl
+++ 3jsJourney/4-44 Procedural Terrain/src/shaders/terrain/fragment.glsl
@@ -1,10 +1,19 @@
-
+uniform vec3 uColorWaterDeep;
+uniform vec3 uColorWaterSurface;
+uniform vec3 uColorSand;
+uniform vec3 uColorGrass;
+uniform vec3 uColorSnow;
+uniform vec3 uColorRock;
 
 
 void main()
 {
 
+//color
+vec3 color = vec3(1.0);
 
+// final color
+csm_DiffuseColor = vec4(color);
 
     
 }
