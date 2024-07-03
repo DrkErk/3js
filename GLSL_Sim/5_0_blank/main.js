92656e733000b3f5e0a84b0b1cc32009a002bdd4
ED <erikdrk@gmail.com>
2024-07-03 16:04:48 -0400

    3js glsl sec5 done

--- GLSL_Sim/5_0_blank/main.js
+++ GLSL_Sim/5_0_blank/main.js
@@ -19,7 +19,8 @@ class SimonDevGLSLCourse {
     this.camera_.position.set(0, 0, 1);
 
     await this.setupProject_();
-    
+
+    this.previousRAF_ = null;
     this.onWindowResize_();
     this.raf_();
   }
@@ -28,17 +29,26 @@ class SimonDevGLSLCourse {
     const vsh = await fetch('./shaders/vertex-shader.glsl');
     const fsh = await fetch('./shaders/fragment-shader.glsl');
 
+    const loader = new THREE.TextureLoader();
+    const dogTexture = loader.load('./textures/dog.jpg');
+
     const material = new THREE.ShaderMaterial({
-      uniforms: {},
+      uniforms: {
+        diffuse1: { value: dogTexture },
+        time: {value: 0.0},
+      },
       vertexShader: await vsh.text(),
       fragmentShader: await fsh.text()
     });
+
+    this.material_ = material;
 
     const geometry = new THREE.PlaneGeometry(1, 1);
     const plane = new THREE.Mesh(geometry, material);
     plane.position.set(0.5, 0.5, 0);
     this.scene_.add(plane);
 
+    this.totalTime_ = 0.0;
     this.onWindowResize_();
   }
 
@@ -48,9 +58,21 @@ class SimonDevGLSLCourse {
 
   raf_() {
     requestAnimationFrame((t) => {
+      if (this.previousRAF_ === null) {
+        this.previousRAF_ = t;      // setting previous request animation frame to current time
+      }
+
+      this.step_(t - this.previousRAF_); // call the step everytime raf is called will lead to accumulation error
       this.threejs_.render(this.scene_, this.camera_);
       this.raf_();
+      this.previousRAF_ = t;
     });
+  }
+
+  step_(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001; // set
+    this.totalTime_ += timeElapsedS; // total time in project setup is incremented by time elapsed
+    this.material_.uniforms.time.value = this.totalTime_; //
   }
 }
 
