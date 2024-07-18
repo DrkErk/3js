dc6a18270f51dcfb9311b7a042bc2daf88b9d280
ED <erikdrk@gmail.com>
2024-07-17 23:36:08 -0400

    glsl chap 6 simple transforms done

--- GLSL_Sim/7_0_blank/shaders/vertex-shader.glsl
+++ GLSL_Sim/7_0_blank/shaders/vertex-shader.glsl
@@ -14,9 +14,29 @@ float remap(float v, float inMin, float inMax, float outMin, float outMax) {
   return mix(outMin, outMax, t);
 }
 
+//based on linear algebra for rotation of an obj requires use of a 3dmatrix
+mat3 rotateY(float radians)
+{
+  float s = sin(radians);
+  float c = cos(radians);
+
+  return mat3(
+    c, 0.0, s,
+    0.0, 1.0, 0.0
+    -s, 0.0, c
+  );
+}
 
 void main() {	
-  vec3 localSpacePosition = position;
+  // THE RESULTS ARE NON COMMUNITATIVE, ORDER MATTERS!!!!!!
+  // Everything starts at the origin!!!!!
+  //
+  vec3 localSpacePosition = position; // position is brought in from the obj itself
+  //localSpacePosition.z += sin(time); // makes the obj move back and forth on the z
+  // We then need the remap func for scaling
+  //localSpacePosition.xz *= remap(sin(time), -1.0, 1.0, 0.5, 1.5);
+  // ROTATION
+  localSpacePosition = rotateY(time) * localSpacePosition; 
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.0); // vec4 because w coord is like a scaling value
   // 1 would be positional coords and 0 would be coord vectors
