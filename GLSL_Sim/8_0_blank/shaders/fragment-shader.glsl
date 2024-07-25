992bd8561ee3c9ce8dc1495137aeede50954c319
ED <erikdrk@gmail.com>
2024-07-24 20:22:29 -0400

    glsl chap 8P1 DONE

--- GLSL_Sim/8_0_blank/shaders/fragment-shader.glsl
+++ GLSL_Sim/8_0_blank/shaders/fragment-shader.glsl
@@ -13,10 +13,33 @@ float remap(float v, float inMin, float inMax, float outMin, float outMax) {
   return mix(outMin, outMax, t);
 }
 
-void main() {
-  vec2 pixelCoords = (vUvs - 0.5) * resolution;
+vec3 drawGrid(vec3 colour, vec3 lineColour, float cellSpacing, float lineWidth) {
+  vec2 center = vUvs - 0.5;
+  vec2 cellPosition = abs(fract(center * resolution / vec2(cellSpacing)) - 0.5);
+  float distToEdge = (0.5 - max(cellPosition.x, cellPosition.y)) * cellSpacing;
+  float lines = smoothstep(0.0, lineWidth, distToEdge);
+
+  colour = mix(lineColour, colour, lines);
+
+  return colour;
+}
+
+vec3 backgroundColor() {
+  float distFromCenter = length(abs(vUvs));
   
-  vec3 colour = vec3(0.0);
+  float vignette = 1.0 - distFromCenter;
+  vignette = smoothstep(0.0, 0.7, vignette);
+  vignette = remap(vignette, 0.0, 1.0, 0.3, 1.0);
+
+  return vec3(vignette);
+}
+
+void main() {
+  vec2 pixelCoords = (vUvs - 0.5) * resolution; // to work in pixel coords
+  
+  vec3 colour = backgroundColor(0.0);
+  colour = drawGrid(colour, vec3(0.5), 10.0, 1.0);
+  colour = drawGrid(colour, vec3(0.0), 100.0, 2.0);
 
   gl_FragColor = vec4(colour, 1.0);
 }
