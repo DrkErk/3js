ed35af8bb78448f764bbc0037be28137b56092e4
ED <erikdrk@gmail.com>
2024-07-25 22:51:10 -0400

    glsl 8 to transforms

--- GLSL_Sim/8_0_blank/shaders/fragment-shader.glsl
+++ GLSL_Sim/8_0_blank/shaders/fragment-shader.glsl
@@ -34,12 +34,46 @@ vec3 backgroundColor() {
   return vec3(vignette);
 }
 
+float sdfLine(vec2 p, vec2 a, vec2 b)
+{
+  vec2 pa = p - a;
+  vec2 ba = b - a;
+  float h = clamp(dot(pa,ba) / dot(ba, ba), 0.0, 1.0);
+
+  return length(pa - ba * h);
+}
+
+float sdfCircle(vec2 p, float r)
+{
+  return length(p) - r;
+}
+
+float sdfBox(vec2 p, vec2 b)
+{
+  vec2 d = abs(p) - b;
+  return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+}
+
+float sdfHexagon( in vec2 p, in float r)
+{
+  const vec3 k = vec3(-0.866025404, 0.5, 0.577350269);
+  p = abs(p);
+  p -= 2.0 * min(dot(k.xy, p), 0.0) * k.xy;
+  p -= vec2(clamp(p.x, -k.z * r, k.z * r), r);
+  return length(p) * sign(p.y);
+}
+
 void main() {
   vec2 pixelCoords = (vUvs - 0.5) * resolution; // to work in pixel coords
   
   vec3 colour = backgroundColor(0.0);
   colour = drawGrid(colour, vec3(0.5), 10.0, 1.0);
   colour = drawGrid(colour, vec3(0.0), 100.0, 2.0);
+
+  //float d = sdfCircle(pixelCoords, 100.0);
+  //float d = sdfLine(pixelCoords, vec2(-100.0, -50.0), vec2(200.0, -75.0));
+  float d = sdfBox(pixelCoords, vec2(300.0, 100.0));
+  colour = mix(RED, colour, step(5.0, d)); // step(0.0, d); for the circle // step(5.0, d)); for the line thickness
 
   gl_FragColor = vec4(colour, 1.0);
 }
